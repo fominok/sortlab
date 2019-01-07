d2bbf6b18669bbfebc44ab22402e30766dda599d
Evgeny Fomin <fominok@hotmail.com>
2019-01-07 15:51:35 +0300

    radix sort for i32

--- src/counting_sort.rs
+++ src/counting_sort.rs
@@ -1,9 +1,11 @@
-use byteorder::{LittleEndian, WriteBytesExt};
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 use std::default::Default;
+use std::io::Cursor;
 
 pub trait HasSortData {
     fn bytes_count() -> u8;
     fn bytes(num: Self) -> Vec<u8>;
+    fn is_signed() -> bool;
 }
 
 impl HasSortData for u32 {
@@ -16,6 +18,27 @@ impl HasSortData for u32 {
         wtr.write_u32::<LittleEndian>(num)
             .expect("What could go wrong...");
         wtr
+    }
+
+    fn is_signed() -> bool {
+        false
+    }
+}
+
+impl HasSortData for i32 {
+    fn bytes_count() -> u8 {
+        4
+    }
+
+    fn bytes(num: i32) -> Vec<u8> {
+        let mut wtr = vec![];
+        wtr.write_i32::<LittleEndian>(num)
+            .expect("What could go wrong...");
+        wtr
+    }
+
+    fn is_signed() -> bool {
+        true
     }
 }
 
@@ -34,6 +57,13 @@ impl GetSortData {
     {
         T::bytes(num)
     }
+
+    fn is_signed<T>() -> bool
+    where
+        T: HasSortData,
+    {
+        T::is_signed()
+    }
 }
 
 pub fn sort<T>(coll: &Vec<T>) -> Vec<T>
@@ -41,11 +71,21 @@ where
     T: HasSortData + Copy + Default,
 {
     let mut v = coll.clone();
-    for i in 0..GetSortData::bytes_count::<u32>() {
+    let bc = GetSortData::bytes_count::<T>();
+    let is_signed = GetSortData::is_signed::<T>();
+    for i in 0..bc {
         let mut v2 = vec![Default::default(); v.len()];
         let v_byte = v
             .iter()
-            .map(|&x| GetSortData::bytes(x)[i as usize])
+            .map(|&x| {
+                let byte = GetSortData::bytes(x)[i as usize];
+                if is_signed && (i == bc - 1) {
+                    let i8_byte = Cursor::new(vec![byte]).read_i8().unwrap();
+                    (i8_byte as i16 + 128) as u8
+                } else {
+                    byte
+                }
+            })
             .collect::<Vec<u8>>();
 
         let mut counter = [0; 256];
@@ -65,7 +105,24 @@ where
 
 #[test]
 fn positives_test() {
-    let v = vec![1723, 456, 76, 85, 431, 31, 904];
+    let v: Vec<u32> = vec![1723, 456, 76, 85, 431, 31, 904];
     let v1 = sort(&v);
     assert_eq!(v1, vec![31, 76, 85, 431, 456, 904, 1723]);
+}
+
+#[test]
+fn negatives_test() {
+    let v: Vec<i32> = vec![-1337, -1338, -420, -1420];
+    let v1 = sort(&v);
+    assert_eq!(v1, vec![-1420, -1338, -1337, -420]);
+}
+
+#[test]
+fn all_in() {
+    let v: Vec<i32> = vec![-1337, 345, -1338, 0, -420, 0, -1420, 420, 4, 3, -9999, 432];
+    let v1 = sort(&v);
+    assert_eq!(
+        v1,
+        vec![-9999, -1420, -1338, -1337, -420, 0, 0, 3, 4, 345, 420, 432]
+    );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
 mod counting_sort;
 
-fn main() {
-    let v = vec![1723, 456, 76, 85, 431, 31, 904];
-    let v2 = crate::counting_sort::sort(&v);
-
-    println!("Yo: {:?}", v2);
-}
+fn main() {}
